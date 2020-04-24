251aabce034cd04bb6a2e87e902ff3abb286978b
Johannes Häggqvist <johannes@eatcreatesleep.net>
2020-04-24 17:53:42 +0200

    Force landscape mode and hide status and navigationbars

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,15 @@ new Vue({
     store,
     render: h => h(App),
 }).$mount('#app');
+
+declare global {
+    interface Window {
+        navigationbar: any;
+    }
+}
+
+document.addEventListener('deviceready', () => {
+    window.screen.orientation.lock('landscape');
+    window.navigationbar.setUp(true);
+    StatusBar.hide();
+});
