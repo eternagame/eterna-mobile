a64ac1afe940d9f8d02bd5ec784110774279dec5
Johannes Häggqvist <johannes@eatcreatesleep.net>
2020-05-14 16:00:56 +0200

    Remove back to home overlay button and listen to navigate requests from the game iframe

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,15 +1,21 @@
 <template>
 <div id="game-container">
     <iframe id="game" title="Game" :src="gameUrl"></iframe>
-    <div id="overlay">
-        <router-link to="/" exact>Back to Home</router-link>
-    </div>
 </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
+    mounted() {
+        const gameFrame = document.getElementById('game');
+        if (gameFrame) {
+            gameFrame.addEventListener('navigate', e => {
+                const location: string = (<CustomEvent>e).detail;
+                this.$router.replace(location);
+            });
+        }
+    },
     computed: {
         gameUrl() {
             const puzzle = this.$route.params.puzzle;
@@ -28,14 +34,5 @@ export default Vue.extend({
     width: 100vw;
     height: 100vh;
     border: 0;
-}
-
-#overlay {
-    position: absolute;
-    right: 10px;
-    top: 10px;
-    background-color: rgba(112, 112, 112, 0.671);
-    padding: 2px 8px;
-    border-radius: 4px;
 }
 </style>
