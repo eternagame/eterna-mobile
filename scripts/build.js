551439f5f72fe132247a99f6a1ccf27436307fef
Jonathan Romano <jonathan@luxaritas.com>
2020-12-15 22:35:28 -0500

    Fix for dev build

--- scripts/build.js
+++ scripts/build.js
@@ -1,33 +1,18 @@
 const runScript = require('@npmcli/run-script');
 
 module.exports = async context => {
-    if (context.opts.options.release) {
-        // Production
-        await runScript({
-            event: 'prebuild:prod',
-            path: '.'
-        });
-        await runScript({
-            event: 'build:prod',
-            path: '.',
-        });
-        await runScript({
-            event: 'postbuild:prod',
-            path: '.',
-        });
-    } else {
-        // Development
-        await runScript({
-            event: 'prebuild:dev',
-            path: '.',
-        });
-        await runScript({
-            event: 'build:dev',
-            path: '.',
-        });
-        await runScript({
-            event: 'postbuild:dev',
-            path: '.',
-        });
-    }
+    const env = context.opts.options.release ? 'prod' : 'dev';
+    console.log(`Running build scripts for env ${env}`);
+    await runScript({
+        event: `prebuild:${env}`,
+        path: '.'
+    });
+    await runScript({
+        event: `build:${env}`,
+        path: '.',
+    });
+    await runScript({
+        event: `postbuild:${env}`,
+        path: '.',
+    });
 };
