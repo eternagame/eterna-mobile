1cee43b8c0a3ad4710c98128d7fbf4d141e94897
Jonathan Romano <jonathan@luxaritas.com>
2024-07-22 18:03:23 -0400

    First pass CI

--- scripts/build.js
+++ scripts/build.js
@@ -1,21 +1,19 @@
 const runScript = require('@npmcli/run-script');
 
 module.exports = async context => {
+    // In our GitHub actions, we'll handle these manually so we can parallelize
+    if (process.env.PARALLEL_BUILD) return;
+
     const env = process.env.NODE_ENV ? process.env.NODE_ENV : context.opts.options.release ? 'prod' : 'dev';
     console.log(`Running build scripts for env ${env}`);
-    await runScript({
-        event: `prebuild:${env}`,
-        path: '.',
-        stdio: 'inherit',
-    });
     await runScript({
         event: `build:${env}`,
         path: '.',
         stdio: 'inherit',
     });
     await runScript({
-        event: `postbuild:${env}`,
-        path: '.',
+        event: `build:${env}`,
+        path: './eternajs',
         stdio: 'inherit',
     });
 };
