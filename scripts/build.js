d129342c9e2e3ff16cd8f29868975adf70383fe0
Jonathan Romano <jonathan@luxaritas.com>
2025-12-15 14:21:51 -0500

    Fix postbuild steps not running when build is triggered via cordova hooks

--- scripts/build.js
+++ scripts/build.js
@@ -7,12 +7,32 @@ module.exports = async context => {
     const env = process.env.NODE_ENV ? process.env.NODE_ENV : context.opts.options.release ? 'prod' : 'dev';
     console.log(`Running build scripts for env ${env}`);
     await runScript({
-        event: `build:${env}`,
+        event: `prebuild:${env}`,
         path: './eternajs',
         stdio: 'inherit',
     });
     await runScript({
         event: `build:${env}`,
+        path: './eternajs',
+        stdio: 'inherit',
+    });
+    await runScript({
+        event: `postbuild:${env}`,
+        path: './eternajs',
+        stdio: 'inherit',
+    });
+    await runScript({
+        event: `prebuild:${env}`,
+        path: '.',
+        stdio: 'inherit',
+    });
+    await runScript({
+        event: `build:${env}`,
+        path: '.',
+        stdio: 'inherit',
+    });
+    await runScript({
+        event: `postbuild:${env}`,
         path: '.',
         stdio: 'inherit',
     });
